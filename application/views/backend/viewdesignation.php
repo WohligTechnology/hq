81098742821faa59ce514db8ee33284423d1f9e9
Pooja <pthakare33@gmail.com>
2015-08-08 12:06:03 +0530

    changes

--- application/views/backend/viewdesignation.php
+++ application/views/backend/viewdesignation.php
@@ -1,6 +1,6 @@
 <div class="row" style="padding:1% 0">
     <div class="col-md-10">
-        <a class="btn btn-primary pull-right" href="<?php echo site_url(" site/createdesignation "); ?>"><i class="icon-plus"></i>Create </a> &nbsp;
+        <a class="btn btn-primary pull-right" href="<?php echo site_url("site/createdesignation"); ?>"><i class="icon-plus"></i>Create </a> &nbsp;
     </div>
     <div class="col-md-2">
 	
